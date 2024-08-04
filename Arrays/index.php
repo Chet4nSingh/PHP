2a9a644c488406420fd1ff60154e0307a3acb044
Chetan Singh <chet4nsingh@gmail.com>
2024-08-04 22:33:19 +0530

    Update ./Arrays/index.php

--- Arrays/index.php
+++ Arrays/index.php
@@ -10,6 +10,8 @@
             background-color: #000;
             color: green;
             display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+            grid-template-rows: 1fr 1fr;
             place-items: center;
             height: 100vh;
             margin: 0;
@@ -19,7 +21,22 @@
 </head>
 <body>
     <?php 
-        
+        $programmingLanguages = ["Java", "Python", "C ++"];
+        echo "<pre>";
+        print_r($programmingLanguages);
+        echo "</pre>";
+
+        // Pushing new element at the end of array
+        $programmingLanguages[] = "JavaScript";
+        echo "<pre>";
+        print_r($programmingLanguages);
+        echo "</pre>";
+
+        // Pushing multiple new elements at the end of array
+        array_push($programmingLanguages, "C", "Go");
+        echo "<pre>";
+        print_r($programmingLanguages);
+        echo "</pre>";
     ?>
 </body>
 </html>
