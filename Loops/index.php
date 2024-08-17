1a7b570c1cf6185ebeece6d828205ea7ff1a4be4
Chetan Singh <chet4nsingh@gmail.com>
2024-08-17 10:07:43 +0530

    Add Loops directory

---
+++ Loops/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conditional Statements</title>
+    <style>
+        body {
+            font-family: monospace;
+            background-color: #000;
+            color: green;
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-size: 2rem;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        
+    ?>    
+</body>
+</html>
