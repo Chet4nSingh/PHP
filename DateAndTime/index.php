fa3658d8f52816ab5fa090992a91d945401eb964
Chetan Singh <chet4nsingh@gmail.com>
2024-08-19 23:22:12 +0530

    Add DateAndTime directory

---
+++ DateAndTime/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conditional Statements</title>
+    <style>
+        body {
+            font-family: monospace;
+            background-color: #000;
+            color: green;
+            display: grid;
+            place-items: center;
+            min-height: 100vh;
+            margin: 0;
+            font-size: 2rem;
+        }
+
+        pre {
+            padding: 4rem;
+            border: 1px dashed green;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        
+    ?>    
+</body>
+</html>
