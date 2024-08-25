03eae09eb6ceac9a2e9c06eaf95102fad2c7fbed
Chetan Singh <chet4nsingh@gmail.com>
2024-08-25 23:37:00 +0530

    Add FileSystem directory

---
+++ FileSystem/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INT220</title>
+    <style>
+        body {
+            font-family: monospace;
+            background-color: #000;
+            color: green;
+            display: grid;
+            /* grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr 1fr; */
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-size: 2rem;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        
+    ?>    
+</body>
+</html>
