09814dc7bfca32c4ae8948b3204f37471bc9fa35
Chetan Singh <chet4nsingh@gmail.com>
2024-09-03 22:10:55 +0530

    Update ./DateAndTime/index.php

--- DateAndTime/index.php
+++ DateAndTime/index.php
@@ -24,6 +24,8 @@
 </head>
 <body>
     <?php
+        /* 
+        
         $currentTime = time();
         echo $currentTime; //* Today
         echo "<br>";
@@ -39,7 +41,29 @@
         date_default_timezone_set('UTC');
         echo date('d/m/Y g:ia')."<br>";
         echo date('d/m/Y g:ia', $currentTime + 5 * 24 * 60 * 60)."<br>";
-        echo date('d/m/Y g:ia', $currentTime - 24 * 60 * 60)."<br>";
+        echo date('d/m/Y g:ia', $currentTime - 24 * 60 * 60)."<br>"; 
+        
+        */
+
+        // Current date and time
+        echo date('Y-m-d H:i:s');  // Outputs: 2024-09-02 12:45:30 (example)
+
+        // Current timestamp
+        echo time();  // Outputs: 1693646730 (current Unix timestamp)
+
+        // Formatting date and time
+        echo date('l, F j, Y');  // Outputs: Monday, September 2, 2024
+
+        // Creating a specific date and time
+        $date = mktime(15, 20, 0, 9, 2, 2024);
+        echo date('Y-m-d H:i:s', $date);  // Outputs: 2024-09-02 15:20:00
+
+        // Converting a string to time
+        $dateString = '2024-12-25 10:00:00';
+        $timestamp = strtotime($dateString);
+        echo date('Y-m-d H:i:s', $timestamp);  // Outputs: 2024-12-25 10:00:00
+
+        
     ?>    
 </body>
 </html>
