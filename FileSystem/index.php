4f73686c063b426a12685366b9b36cecb98536e0
Chetan Singh <chet4nsingh@gmail.com>
2024-09-03 22:45:50 +0530

    Update ./FileSystem/index.php

--- FileSystem/index.php
+++ FileSystem/index.php
@@ -21,10 +21,34 @@
 </head>
 <body>
     <?php
+        /*
         $dir = scandir(__DIR__);
         print_r($dir);
         echo "<br>";
         echo is_file($dir[2])? "true": "false";
+        */
+
+        // Reading a file
+        $content = file_get_contents('example.txt');
+        echo $content;
+
+        // Writing to a file
+        file_put_contents('example.txt', 'Hello, World!');
+
+        // Appending to a file
+        file_put_contents('example.txt', 'Append this text.', FILE_APPEND);
+
+        // Checking if a file exists
+        if (file_exists('example.txt')) {
+            echo 'File exists.';
+        }
+
+        // Deleting a file
+        unlink('example.txt');
+
+        // Creating a directory
+        mkdir('new_directory');      
+
     ?>    
 </body>
 </html>
