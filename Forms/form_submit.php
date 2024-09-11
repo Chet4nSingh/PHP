dd5327596addda089a6cdee25de527e94a8c047e
Chetan Singh <chet4nsingh@gmail.com>
2024-09-11 19:07:36 +0530

    Update Forms directory

---
+++ Forms/form_submit.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forms</title>
+    <style>
+        body {
+            font-family: monospace;
+            background-color: #000;
+            color: green;
+            display: grid;
+            /* grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr 1fr; */
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-size: 2rem;
+        }
+
+        div {
+            width: 40%;
+            padding: 2rem;
+            border: 1px dashed green;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <?php 
+
+            echo "$_POST[name]";
+        ?>
+    </div>
+</body>
+</html>
