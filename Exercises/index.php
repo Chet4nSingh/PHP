a452a88c03f6d9c204acc37d4de41e368b44acbe
Chetan Singh <chet4nsingh@gmail.com>
2024-09-15 23:59:32 +0530

    Update ./Exercises/index.php

--- Exercises/index.php
+++ Exercises/index.php
@@ -4,8 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercises</title>
+    <style>
+        body {
+            font-family: monospace;
+            background-color: #000;
+            color: white;
+            padding: 4rem;
+            height: 100vh;
+            margin: 0;
+            font-size: 2rem;
+        }
+
+        a {
+            text-decoration: none;
+            color: white;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
 </head>
 <body>
-    <a href="getRemainder.php">Remainder</a>
+    <h1>Exercises</h1>
+    <ul>
+        <li><a href="getRemainder.php">Remainder</a></li>
+    </ul>
 </body>
 </html>
